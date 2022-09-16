d63e64bed807bd4bbd7734733c1147305fbd498e
Hendrik Berends <9317713-HENNIROCKS@users.noreply.gitlab.com>
2022-09-16 12:00:04 +0200

    Added blueprint fields to index php file and changed version in composer json to tag v1.0.3

--- index.php
+++ index.php
@@ -2,6 +2,18 @@
 
 Kirby::plugin('foerdeliebe/head', [
     'blueprints' => [
+        'head/head_author' => __DIR__ . '/blueprints/fields/head_author.yml',
+        'head/head_description' => __DIR__ . '/blueprints/fields/head_description.yml',
+        'head/head_google_site_verification' => __DIR__ . '/blueprints/fields/head_google_site_verification.yml',
+        'head/head_keywords' => __DIR__ . '/blueprints/fields/head_keywords.yml',
+        'head/head_og_description' => __DIR__ . '/blueprints/fields/head_og_description.yml',
+        'head/head_og_image' => __DIR__ . '/blueprints/fields/head_og_image.yml',
+        'head/head_og_title' => __DIR__ . '/blueprints/fields/head_og_title.yml',
+        'head/head_robots' => __DIR__ . '/blueprints/fields/head_robots.yml',
+        'head/head_twitter_description' => __DIR__ . '/blueprints/fields/head_twitter_description.yml',
+        'head/head_twitter_image' => __DIR__ . '/blueprints/fields/head_twitter_image.yml',
+        'head/head_twitter_site' => __DIR__ . '/blueprints/fields/head_twitter_site.yml',
+        'head/head_twitter_title' => __DIR__ . '/blueprints/fields/head_twitter_title.yml',
         'head/site' => __DIR__ . '/blueprints/tabs/site.yml',
         'head/page' => __DIR__ . '/blueprints/tabs/page.yml',
     ],
